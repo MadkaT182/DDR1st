5ede2f392355bf42c9f0f9d423d305db6d1b7f3c
unknown <madcat182@gmail.com>
2020-03-25 16:09:02 -0500

    General update

---
+++ BGAnimations/ScreenOptionsHome underlay/default.lua
@@ -0,0 +1,25 @@
+local t = Def.ActorFrame {
+	OnCommand=cmd(addx,-48;addy,-48);
+};
+
+for j = 1,5 do
+	for i = 1,9 do
+		t[#t+1] = Def.ActorFrame{
+			LoadActor("star")..{
+				InitCommand=cmd(addx,i*96;addy,j*96;spin;effectmagnitude,0,0,-70;diffusealpha,.5);
+			};
+		};
+	end;
+end;
+
+t[#t+1] = Def.ActorFrame{
+	InitCommand=cmd(addx,48;addy,48);
+	Def.Quad{
+		InitCommand=cmd(FullScreen;rainbow;effectperiod,10;blend,"BlendMode_Modulate");
+	};
+	LoadActor("separator")..{
+		InitCommand=cmd(Center);
+	};
+};
+
+return t;
